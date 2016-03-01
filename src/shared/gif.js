879f344beab54d65ed7fd01b4d8bfc5113cf6856
Damien BRY <damienbry@MacBook-Pro-de-Damien-2.local>
2016-03-01 11:30:52 +0100

    fix(gif): component that replies with a random gif

---
+++ src/shared/gif.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var request = require('request');
+var Promise = require('bluebird');
+
+var endpoint = 'http://api.giphy.com/v1/gifs';
+var api_key  = 'dc6zaTOxFJmzC';
+
+module.exports = {
+
+    getRandomGIF: function(){
+        return new Promise(function(resolve, reject){
+            request(endpoint+'/random?api_key='+api_key+'&tag=reaction', function(error, response, body){
+                if(error){
+                    reject(error);
+                }
+                if(response.statusCode != 200){
+                    reject(response.statusCode);
+                }
+
+                var json = JSON.parse(body);
+                resolve(json.data.image_url);
+            });
+        });
+    }
+
+};
